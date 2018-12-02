86059e41fb87a00e35143e463435b61cf0c18527
Shea Belsky <sheabelsky@gmail.com>
2018-12-01 22:16:45 -0500

    Capacitive Touch + RGB Ring

--- capacativeTouch.ino
+++ capacativeTouch.ino
@@ -1,19 +1,22 @@
-/*************************************************** 
-  This is a library for the CAP1188 I2C/SPI 8-chan Capacitive Sensor
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
 
-  Designed specifically to work with the CAP1188 sensor from Adafruit
-  ----> https://www.adafruit.com/products/1602
+// RGB Ring Includes
+#define RGB_PIN A5
 
-  These sensors use I2C/SPI to communicate, 2+ pins are required to  
-  interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
+int numPins = 24;
+int defaultWait = defaultWait;
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
- 
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(numPins, RGB_PIN, NEO_GRB + NEO_KHZ800);
+
+// Capacative Touch Includes
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_CAP1188.h>
@@ -38,18 +41,20 @@
 // Use I2C, no reset pin!
 Adafruit_CAP1188 cap = Adafruit_CAP1188();
 
-// Or...Use I2C, with reset pin
-//Adafruit_CAP1188 cap = Adafruit_CAP1188(CAP1188_RESET);
+// Variable to maintain the state of the last touched area
+int touchedArea;
 
-// Or... Hardware SPI, CS pin & reset pin 
-// Adafruit_CAP1188 cap = Adafruit_CAP1188(CAP1188_CS, CAP1188_RESET);
-
-// Or.. Software SPI: clock, miso, mosi, cs, reset
-//Adafruit_CAP1188 cap = Adafruit_CAP1188(CAP1188_CLK, CAP1188_MISO, CAP1188_MOSI, CAP1188_CS, CAP1188_RESET);
+// Variable to maintain the state of the ring color
+uint32_t ringColor;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("CAP1188 test!");
+    
+  // Begin the RGB strip
+  strip.begin();
+  strip.setBrightness(20);
+  // Set all the pixels to be off
+  strip.show();
 
   // Initialize the sensor, if using i2c you can pass in the i2c address
   // if (!cap.begin(0x28)) {
@@ -61,19 +66,126 @@ void setup() {
 }
 
 void loop() {
+  // Some example procedures showing how to display to the pixels:
+  // colorWipe(strip.Color(255, 0, 0), 0); // Red
+
+  // Capacative touch
   uint8_t touched = cap.touched();
 
   if (touched == 0) {
     // No touch detected
     return;
   }
-  
+
+  // Determine which section of the capacative touch sensor was touched, if any
   for (uint8_t i=0; i<8; i++) {
     if (touched & (1 << i)) {
-      Serial.print("C"); Serial.print(i+1); Serial.print("\t");
+      touchedArea = i+1;
     }
   }
-  Serial.println();
-  delay(50);
+
+  switch(touchedArea) {
+    case 1:
+      ringColor = strip.Color(255, 0, 0);
+      break;
+    case 2:
+      ringColor = strip.Color(0, 255, 0);
+      break;
+    case 3:
+      ringColor = strip.Color(0, 0, 255);
+      break;
+    case 4:
+      ringColor = strip.Color(255, 255, 0);
+      break;
+    default:
+      Serial.println(touchedArea);
+      break;
+  }
+  colorWipe(ringColor, defaultWait);
+  delay(defaultWait);
 }
 
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
